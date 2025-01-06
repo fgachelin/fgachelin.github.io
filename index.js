16a3b7786eb161ec3c1fbe1a2cb442077f41c64a
Francois Gachelin <64481278+fgachelin@users.noreply.github.com>
2025-01-06 10:01:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ let affiche = function(data){
   document.body.innerHTML += d 
 }
 
-fetch('./index.md', {})
+//fetch('./index.md', {})
+fetch('http://194.167.100.167:8080/gachelin.web/terminale_nsi/index.md', {})
   .then(response => response.text())
   .then(data=> affiche(data))
   .catch(error => console.error(error))

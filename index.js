83db78569e0ff3ea9de1439a978379d8ab26bc6b
Francois Gachelin <64481278+fgachelin@users.noreply.github.com>
2025-01-06 03:22:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ fetch('./index.md', {})
   .then(response => response.text())
   .then(data=> affiche(data))
   .catch(error => console.error(error))
+
+const name = 'JSX'
+const element = <h1>Super, ce {name}</h1>
+affiche(element)

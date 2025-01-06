919ad2668acb357b58b098679800b31afa6e2446
Francois Gachelin <64481278+fgachelin@users.noreply.github.com>
2025-01-06 02:46:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let affiche = function(t){document.body.innerHTML += "<p>"+t+"</p>";
+let affiche = function(t){document.body.innerHTML += "<p>"+t+"</p>"}
 
-fetch('./test.md', {mode: 'no-cors'})
+fetch('./test.md', {})
   .then(response => response.text())
   .then(data=> affiche(data))
-  .catch(error => console.error(error));
+  .catch(error => console.error(error))

7e280a6b04e1e74a290bb60fb75a64a3dd9b1111
Francois Gachelin <64481278+fgachelin@users.noreply.github.com>
2025-02-11 11:55:14 +0100

    Update numphy.js

--- numphy.js
+++ numphy.js
@@ -1,3 +1,3 @@
 let titre = "Numphy github cdn file !"
-document.body.innerHTML += `<h1>${titre}</h1>`
+document.body.innerHTML = document.body.innerHTML + `<h1>${titre}</h1>`
 console.log("Numphy github cdn file !")

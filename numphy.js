927f8a5bbea5e6a2e94e5e5f126cfa96fbc800af
Francois Gachelin <64481278+fgachelin@users.noreply.github.com>
2025-02-11 13:18:08 +0100

    Update numphy.js

--- numphy.js
+++ numphy.js
@@ -1,5 +1,16 @@
-/*Fichier test pour cdn jsdelivr*/
-let titre = "Numphy github cdn file v2 !"
-document.body.innerHTML = document.body.innerHTML + `<h1>${titre}</h1>`
-console.log("Numphy github cdn file !")
-alert("Numphy github cdn file !")
+function mmd2html(dom_mmd)
+{
+    let mmd = dom_mmd.innerHTML
+    let lignes = mmd.split("\n")
+    let html = ''
+    for(let ligne of lignes)
+    {
+        html += `<p style='color:red'>${ligne}</p>`
+    }
+    return `<h1 style='color:red'>mmd2html</h1>${html}`
+}
+
+for(let mmd of document.querySelectorAll('mmd'))
+{   
+    mmd.innerHTML = mmd2html(mmd)
+}
